893cf5c08a8658cae21669b91d0dfa5bb5ab0315
vrizas <vrizasizza@gmail.com>
2022-06-04 15:14:46 +0700

    delete cloud storage, delete mongoose

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ const bundledRoutes = (() => ({
     name: 'routes',
     version: '1.0.0',
     register: (server, options) => {
-      server.route(routes.flat().map((route) => ({ ...route, path: route.path })));
+      server.route(routes.flat());
     },
   }))();
   
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,18 +1,15 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 const { getAuth } = require('firebase-admin');
-const { getStorage } = require('firebase-admin/storage');
 const serviceAccount = require('../config/serviceAccountKey.json');
 
 const init = async () => {
   initializeApp({
     credential: cert(serviceAccount),
-    storageBucket: 'gs://coba-28e96.appspot.com'
   });
   const db = getFirestore();
-  const bucket = getStorage().bucket();
   return {
-    firestore: { db, Timestamp, FieldValue, getAuth, bucket },
+    firestore: { db, Timestamp, FieldValue, getAuth },
   };
 };
 
