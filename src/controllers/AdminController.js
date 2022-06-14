c515d7bbe015975876904b3e2b7a5b662e92ccc3
ardimasyrofi <ardimasyrofi22@gmail.com>
2022-06-14 20:58:20 +0700

    add admin controller

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -1,3 +1,127 @@
+const verifyUser = require('../middlewares/verifyUser');
+
+exports.getAllUsers = async (request, h) => {
+    const { db } = request.server.app.firestore;
+    const { boom } = request.server.app;
+
+    try {
+        const users = await db.collection('users').get();
+        const response = h.response({
+            status: 'success',
+            message: 'All Users retrieved successfully',
+            users: users.docs.map(doc => {
+                return {
+                    id: doc.uid,
+                    ...doc.data()
+                }  
+            })
+        }).code(200);
+        return response;
+    } catch (error) {
+        const response = boom.badRequest(error);
+        return response;
+    }
+};
+
+//Search By Username & Role
+exports.searchUser = async (request, h) => {
+    const { username, role } = request.params;
+    const { db } = request.server.app.firestore;
+    const { boom } = request.server.app;
+
+    try{
+        const result = await db.collection('users').get();
+        const users = [];
+        result.docs.forEach(doc => {
+            const nameUser = doc.data().username.toLowerCase();
+            const usernameLoweredCase = username.toLowerCase();
+            const roleUser = doc.data().role.toLowerCase();
+            const RoleLoweredCase = role.toLowerCase();
+            if(nameUser.includes(usernameLoweredCase)&&roleUser.includes(RoleLoweredCase)){
+                users.push({
+                    uid: doc.uid,
+                    ...doc.data()
+                });
+            }
+        });
+
+        const response = h.response({
+            status: 'success',
+            message: 'User searched successfully',
+            users
+        }).code(200);
+        return response;
+    }catch (error) {
+        const response = boom.badRequest(error);
+        return response;
+    }
+};
+
+//Search By Username
+exports.searchUserByUsername = async (request, h) => {
+    const { username } = request.params;
+    const { db } = request.server.app.firestore;
+    const { boom } = request.server.app;
+
+    try{
+        const result = await db.collection('users').get();
+        const users = [];
+        result.docs.forEach(doc => {
+            const nameUser = doc.data().username.toLowerCase();
+            const usernameLoweredCase = username.toLowerCase();
+            if(nameUser.includes(usernameLoweredCase)){
+                users.push({
+                    uid: doc.uid,
+                    ...doc.data()
+                });
+            }
+        });
+
+        const response = h.response({
+            status: 'success',
+            message: 'User searched successfully',
+            users
+        }).code(200);
+        return response;
+    }catch (error) {
+        const response = boom.badRequest(error);
+        return response;
+    }
+};
+
+//Search User by Role
+exports.searchUserByRole = async (request, h) => {
+    const { role } = request.params;
+    const { db } = request.server.app.firestore;
+    const { boom } = request.server.app;
+
+    try{
+        const result = await db.collection('users').get();
+        const users = [];
+        result.docs.forEach(doc => {
+            const roleUser = doc.data().role.toLowerCase();
+            const RoleLoweredCase = role.toLowerCase();
+            if(roleUser.includes(RoleLoweredCase)){
+                users.push({
+                    uid: doc.uid,
+                    ...doc.data()
+                });
+            }
+        });
+
+        const response = h.response({
+            status: 'success',
+            message: 'User searched successfully',
+            users
+        }).code(200);
+        return response;
+    }catch (error) {
+        const response = boom.badRequest(error);
+        return response;
+    }
+};
+
+
 // const UserModel = require('../models/UserModel');
 // const { getAuth } = require('firebase-admin');
 
--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -4,14 +4,25 @@ const AdminController = require('../controllers/AdminController');
 const routes = [
   {
     method: 'GET',
-    path: '/admin/user/{uid}',
-    handler: AdminController.getUser,
+    path: '/users',
+    handler: AdminController.getAllUsers,
   },
   {
     method: 'GET',
-    path: '/admin/users',
-    handler: AdminController.getAllUsers,
+    path: '/users/{username}/{role}',
+    handler: AdminController.searchUser,
   },
+  {
+    method: 'GET',
+    path: '/users/username/{username}',
+    handler: AdminController.searchUserByUsername,
+  },
+  {
+    method: 'GET',
+    path: '/users/role/{role}',
+    handler: AdminController.searchUserByRole,
+  },
+  
 ];
 
 module.exports = routes;
